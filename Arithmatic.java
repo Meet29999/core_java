fa9c07d7c3d7067951668ca77a3d2a8354c2c609
Meet29999 <109727169+Meet29999@users.noreply.github.com>
2022-07-22 15:00:49 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,14 @@
+class Arithmatic
+{
+ public static void main(String args[])
+{
+ double l,w,side,rectangle,square;
+ l=20;
+ w=10;
+ side=40;
+ square=side*side;
+ System.out.println("area of square is:"+square);
+ rectangle=l*w;
+ System.out.println("area of rectangle is:"+rectangle);
+}
+}
---
+++ DataType.java
@@ -0,0 +1,26 @@
+class DataType
+{
+  public static void main(String args[])
+  {
+    int numl=1500;
+    short myShort=500;
+    byte myByte=40;
+    long myLong=10000000l;
+    float myFloat=26.7f;
+    double myDouble=23.99;
+    char myChar='a';
+    boolean myBool=false;
+    System.out.println("Integer number is:"+numl);
+    System.out.println("Short Integer number is:"+myShort);
+    System.out.println("Byte Integer number is:"+myByte);
+    System.out.println("Long Integer number is:"+myLong);
+    System.out.println("Floating Point number is:"+myFloat);
+    System.out.println("double Floating integer number is:"+myDouble);
+    System.out.println("My First character is:"+myChar);
+    if(myBool==false)
+        {
+    System.out.println("my Boolean data is:"+myBool);
+        }
+}
+}
+ 
